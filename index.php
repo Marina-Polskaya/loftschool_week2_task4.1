6c611dd06e5822b062fba151b8bd0b38099fb33e
Marina Morogova <marina_0880@mail.ru>
2023-07-20 21:34:14 +0700

    Доработан  для применения базового тарифа

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 
 require_once 'TariffInterface.php';
-//require_once 'ServiceInterface.php';
-//require_once 'TariffAbstract.php';
+require_once 'ServiceInterface.php';
+require_once 'TariffAbstract.php';
 require_once 'BasicTariff.php';
-//require_once 'HourlyTariff.php';
-//require_once 'StudentTariff.php';
-//require_once 'ServiceGPS.php';
+require_once 'HourlyTariff.php';
+require_once 'StudentTariff.php';
+require_once 'ServiceGPS.php';
 
 ini_set('display_errors', 'on');
 error_reporting(E_ALL | E_NOTICE);
 
-$tariff = new BasicTariff(5, 1);
-//$tariff->addService(new ServiceGPS(15));
+$tariff = new BasicTariff(5, 60);
+$tariff->addService(new ServiceGPS(15));
 echo $tariff->countTripPrice();
 
