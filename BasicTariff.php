0f0c4ab7eb4e784e7e27ad610d8acdf15175f865
Marina Morogova <marina_0880@mail.ru>
2023-07-20 21:31:55 +0700

    рабочий вариант BasicTariff

--- BasicTariff.php
+++ BasicTariff.php
@@ -2,33 +2,8 @@
 
 require_once 'TariffInterface.php';
 
-class BasicTariff implements TariffInterface
+class BasicTariff extends TariffAbstract
 {
-    protected $pricePerKm;
-    protected $pricePerMin;
-    protected $distance;
-    protected $minutes;
-    
-    public function __construct($distance, $minutes) {
-        $this->pricePerKm = 10;
-        $this->pricePerMin = 3;
-        $this->distance = $distance;
-        $this->minutes = $minutes;
-    }
-    
-    public function countTripPrice() : int
-    {
-        $tripPrice = $this->pricePerKm * $this->distance + $this->pricePerMin * $this->minutes;
-        return $tripPrice;
-    }
-    
-    public function getDistance() : int
-    {
-        return $this->distance;
-    }
-    
-    public function getMinutes() : int
-    {
-        return $this->minutes;
-    }
+    protected $pricePerKm = 10;
+    protected $pricePerMin = 3;
 }
