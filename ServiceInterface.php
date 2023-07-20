ea46028b7d75344f70e3264891d5c67d6db00a74
Marina Morogova <marina_0880@mail.ru>
2023-07-20 21:35:56 +0700

    Добавлен интерфейс услуг

--- ServiceInterface.php
+++ ServiceInterface.php
@@ -1,9 +1,6 @@
 <?php
-/*
+
 interface ServiceInterface
 {
     public function apply(TariffInterface $tariff, &$price);
-    //public function addService(ServiceInterface $service);
-    //public function getDistance() : int;
-    //public function getMinutes() : int;
-}*/
+}
