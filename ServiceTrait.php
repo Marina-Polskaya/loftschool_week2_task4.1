ff87ee838df8194b930640545eea3d403a35d7cb
Marina Morogova <marina_0880@mail.ru>
2023-07-25 19:12:51 +0700

    Добавлен метод getName()

--- ServiceTrait.php
+++ ServiceTrait.php
@@ -1,10 +1,16 @@
 <?php
 
 trait ServiceTrait
-{
+{    
     public function apply(TariffInterface $tariff, &$price)
     {
         $hours = ceil($tariff->getMinutes()/60);
         $price += $this->pricePerHour * $hours;
+        
+    }
+    
+    public function getName() : string
+    {
+        return $this->name;
     }
 }
