061fb2af6cfcd556a2ef83caa5bc920acb0213ca
Marina Morogova <marina_0880@mail.ru>
2023-07-25 19:20:08 +0700

    Исправлено название метода PrintOrderInfo()

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
 <?php
 
 require_once 'TariffInterface.php';
-//require_once 'ServiceTrait.php';
 require_once 'TariffAbstract.php';
 require_once 'BasicTariff.php';
 require_once 'HourlyTariff.php';
 require_once 'StudentTariff.php';
 require_once 'ServiceGPS.php';
-require_once 'ServiceInterface.php';
+require_once 'AddDriver.php';
 
 ini_set('display_errors', 'on');
 error_reporting(E_ALL | E_NOTICE);
 
-$tariff = new BasicTariff(5, 120);
-$tariff->addService(new ServiceGPS());
-echo $tariff->countTripPrice() . '<br />';
+$tariff = new BasicTariff(5, 60);
+$gps = new ServiceGPS();
+$serviceName = $tariff->addService($gps);
+$addDriver = new AddDriver();
+$tariff->addService($addDriver);
+$tariff->printOrderInfo();
+echo '<br />';
 
 $studentTariff = new StudentTariff(25, 45);
 $studentTariff->addService(new ServiceGPS());
-echo $studentTariff->countTripPrice() . '<br />';
+$studentTariff->printOrderInfo();
+echo '<br />';
 
 $hourlyTariff = new HourlyTariff(25, 95);
-echo $hourlyTariff->countTripPrice();
+$hourlyTariff->addService(new AddDriver());
+$hourlyTariff->printOrderInfo();
 
